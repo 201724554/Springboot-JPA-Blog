ff477335144cb1cc164b425be33d7a97107e72b6
201724554 <pay7845@naver.com>
2022-03-22 20:56:30 +0900

    카카오 로그인 1/2

--- src/main/java/com/jin/blog/controller/UserController.java
+++ src/main/java/com/jin/blog/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.jin.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -22,5 +30,31 @@ public class UserController {
 	public String updateForm()
 	{
 		return "user/updateForm";
+	}
+	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) //@ResponseBody -> data를 리턴해주는 컨트롤러 함수
+	{
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type","authorization_code");
+		params.add("client_id","58e9b2e62e635e63b04c19e20acefae5");
+		params.add("redirect_uri","http://localhost:8000/auth/kakao/callback");
+		params.add("code",code);
+		
+		//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+		HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest = new HttpEntity<>(params, header);
+		
+		//Http 요청 후, request에 해당하는 response 받아옴
+		ResponseEntity response = rt.exchange(
+					"https://kauth.kakao.com/oauth/token",
+					HttpMethod.POST,
+					kakaoTokenRequest,
+					String.class //response의 type
+				);
+		return "카카오 토큰 요청 완료, response: " + response;
 	}
 }
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -11,6 +11,7 @@
 			<label for="password">Password:</label> <input type="password" name="password" class="form-control" placeholder="Enter password" id="password">
 		</div>
 		<button id="btn-login" type="submit" class="btn btn-primary">Submit</button>
+		<a href="https://kauth.kakao.com/oauth/authorize?client_id=58e9b2e62e635e63b04c19e20acefae5&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"><img height="38px" src="/image/kakao_login_btn.png"/></a>
 	</form>
 </div>
 <!-- static/js/user.js -->
